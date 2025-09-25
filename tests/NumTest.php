feff2b0e33cd4988246e25b9fc85ce2664cd7a6e
TOBENTOch <info@tobento.ch>
2025-09-25 15:17:59 +0200

    fixed as randomizer throws if same min and max value

--- tests/NumTest.php
+++ tests/NumTest.php
@@ -55,10 +55,6 @@ class NumTest extends TestCase
     {
         $this->assertTrue(is_float(Num::float(min: 1.5, max: 10.5)));
         
-        $this->assertTrue(
-            in_array(Num::float(min: 1, max: 1), [1])
-        );
-        
         $float = Num::float(min: 1.1, max: 2.1);
             
         $this->assertTrue($float >= 1.1 && $float <= 2.1);   
@@ -69,16 +65,13 @@ class NumTest extends TestCase
         $price = Num::price(min: 1.1, max: 2.1);
         $this->assertTrue($price >= 1.1 && $price <= 2.1);
         
-        $price = Num::price(min: 2.333, max: 2.333);        
-        $this->assertSame(2.333, $price);
-        
-        $price = Num::price(min: 2.333, max: 2.333, precision: 2);        
+        $price = Num::price(min: 2.333, max: 2.334, precision: 2);        
         $this->assertSame(2.33, $price);
         
-        $price = Num::price(min: 2.333, max: 2.333, precision: 2, step: 0.05);        
+        $price = Num::price(min: 2.333, max: 2.334, precision: 2, step: 0.05);        
         $this->assertSame(2.35, $price);
         
-        $price = Num::price(min: 2.333, max: 2.333, step: 0.05);        
+        $price = Num::price(min: 2.333, max: 2.334, step: 0.05);        
         $this->assertSame(2.35, $price);        
     }    
 }
